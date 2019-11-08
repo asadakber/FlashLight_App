22879a899ff099af53deac2020e651e7e0b966e3
Muhammed Asad Ali Akbar <asad.akberlogo@gmail.com>
2019-11-08 22:47:53 +0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Flashlight } from '@ionic-native/flashlight/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  isOn: boolean = true
+  constructor(private flashlight: Flashlight) {}
 
-  constructor() {}
+  async isAvailable(): Promise<boolean> {
+    try {
+     return await this.flashlight.available();
+    }
+    catch(e) {
+      console.log(e)
+    }
+  }
 
+  async toggleFlash():Promise<void> {
+    try {
+      let available = await this.isAvailable()
+      if(available) {
+        await this.flashlight.toggle();
+        this.isOn = !this.isOn
+      } else {
+        console.log("Isn't available")
+      }
+    }
+    catch(e) {
+      console.log(e)
+    }
+  }
+
+  async turnOnFlash(): Promise<void> {
+    await this.flashlight.switchOn()
+  }
+
+  async turnOffFlash(): Promise<void> {
+    await this.flashlight.switchOff()
+  }
+
+  async isFlashOn(): Promise<boolean> {
+    return await this.flashlight.isSwitchedOn();
+  }
 }
